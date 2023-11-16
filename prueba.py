93395a8bb1fa16bf7a80226e77a25e28a579b1f2
Ander Ferandez <anderfernandez.ce@gmail.com>
2023-11-16 17:05:51 +0100

    deleted not necessary files

--- prueba.py
+++
@@ -1,7 +0,0 @@
-import requests
-import bs4
-
-page = requests.get("https://gogoanime.dev/category/jujutsu-kaisen-tv-dub")
-parse = bs4.BeautifulSoup(page.content, 'html.parser')
-links = parse.select('.anime_info_body_bg > img')
-print(links)
