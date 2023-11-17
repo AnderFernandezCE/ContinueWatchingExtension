b78eddd69970f4dbe64a85d3605dcf6399c22900
Ander Ferandez <anderfernandez.ce@gmail.com>
2023-11-17 17:15:15 +0100

    refactoring scripts content

--- background.js
+++ background.js
@@ -1,15 +1,21 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   var rootbase = /^https?:\/\/(?:[a-zA-Z0-9-]+\.)?gogoanime3\.net\/$/;
   var subpage = /^https?:\/\/(?:[a-zA-Z0-9-]+\.)?gogoanime3\.net\/(?!anime-list)(?!.*\.html$)[^\/]+$/;
-  if(tab.url && rootbase.test(tab.url)){
+
+  const isPageReady = changeInfo.status === 'complete'
+  const isRootPage = isPageReady && tab.url && rootbase.test(tab.url)
+  const isSubPage = isPageReady && tab.url && subpage.test(tab.url)
+
+  if(isRootPage){
     chrome.tabs.sendMessage(tabId, {
       type: "ROOT"
     });
   }
-  else if(tab.url && subpage.test(tab.url)){
+  else if(isSubPage){
     let anime = tab.url.split("net/")[1].split("-")
     let animeName = anime.slice(0,-2).join("_")
     let animeEpisode = Number(anime.slice(-2)[1])
+    
     chrome.tabs.sendMessage(tabId, {
       type: "EPISODE",
       identifier: "aid_" + animeName,
@@ -17,8 +23,3 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     });
   }
 });
-
-// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-//   console.log(sender)
-//   console.log(request)
-// });
--- main.js
+++ main.js
@@ -1,74 +1,80 @@
 function addElement() {
-  let fragment = document.createDocumentFragment();
+  const indexExists = document.getElementsByClassName("custom-container")[0];
+  if(!indexExists){
+    let fragment = document.createDocumentFragment();
 
-  let continueElement = document.createElement("h1")
-  continueElement.innerText = "CONTINUE WATCHING"
-  // Create a new div element
-  let divElement = document.createElement("div");
-  divElement.classList.add("custom-container");
-
-  // Create a ul element
-  let ulElement = document.createElement("ul");
-  ulElement.classList.add("listContinueWatching");
-
-  // Iterate through localStorage items
-  for (let i in localStorage) {
-    if (i.startsWith("aid_")) {
-      //get storageinfo
-      let data = JSON.parse(localStorage.getItem(i))
-      // Create a li element for each item in localStorage
-      let liElement = document.createElement("li");
-      liElement.classList.add("listStyle");
-      liElement.setAttribute("data-identifier", i);
-
-      // Create a div element inside the li
-      let aInsideLi = document.createElement("a");
-      aInsideLi.href = "https://gogoanime3.net/"+  i.split("_").slice(1).join("-") + "-episode-" + data.episode
-      aInsideLi.classList.add("imgContainer")
-
-      // Create an img element inside the div
-      let imgElement1 = document.createElement("img");
-      imgElement1.src = data.imglink; 
-      imgElement1.classList.add("coverImage")
-
-      // Append the img element to the div
-      aInsideLi.appendChild(imgElement1);
-
-      let divSecond = document.createElement("div");
-      divSecond.classList.add("secondDiv")
-      // Create a paragraph element inside the li
-      let paragraphElement = document.createElement("p");
-      paragraphElement.textContent = i.split("_").slice(1).join(" "); // Set the text content
-
-      // Create a second img element inside the second div
-      let imgElement2 = document.createElement("img");
-      imgElement2.src = chrome.runtime.getURL("assets/remove.png");
-      imgElement2.classList.add("adding-btn")
-      imgElement2.addEventListener("click", (event) => deleteAnimeHandler(i))
-
-      divSecond.appendChild(paragraphElement)
-      // Append the second img element to the second div
-      divSecond.appendChild(imgElement2);
-
-      // Append the div to the li
-      liElement.appendChild(aInsideLi);
-
-      liElement.appendChild(divSecond);
-      
-      // Append the li to the ul
-      ulElement.appendChild(liElement);
+    let continueElement = document.createElement("h1")
+    continueElement.innerText = "CONTINUE WATCHING"
+    // Create a new div element
+    let divElement = document.createElement("div");
+    divElement.classList.add("custom-container");
+  
+    // Create a ul element
+    let ulElement = document.createElement("ul");
+    ulElement.classList.add("listContinueWatching");
+  
+    // Iterate through localStorage items
+    for (let i in localStorage) {
+      if (i.startsWith("aid_")) {
+        //get storageinfo
+        let data = JSON.parse(localStorage.getItem(i))
+        // Create a li element for each item in localStorage
+        let liElement = document.createElement("li");
+        liElement.classList.add("listStyle");
+        liElement.setAttribute("data-identifier", i);
+  
+        // Create a div element inside the li
+        let aInsideLi = document.createElement("a");
+        aInsideLi.href = "https://gogoanime3.net/"+  i.split("_").slice(1).join("-") + "-episode-" + data.episode
+        aInsideLi.classList.add("imgContainer")
+  
+        // Create an img element inside the div
+        let imgElement1 = document.createElement("img");
+        imgElement1.src = data.imglink; 
+        imgElement1.classList.add("coverImage")
+  
+        // Append the img element to the div
+        aInsideLi.appendChild(imgElement1);
+  
+        let divSecond = document.createElement("div");
+        divSecond.classList.add("secondDiv")
+        // Create a paragraph element inside the li
+        let paragraphElement = document.createElement("p");
+        paragraphElement.textContent = i.split("_").slice(1).join(" "); // Set the text content
+  
+        // Create a second img element inside the second div
+        let imgElement2 = document.createElement("img");
+        imgElement2.src = chrome.runtime.getURL("assets/remove.png");
+        imgElement2.classList.add("adding-btn")
+        imgElement2.addEventListener("click", (event) => deleteAnimeHandler(i))
+  
+        divSecond.appendChild(paragraphElement)
+        // Append the second img element to the second div
+        divSecond.appendChild(imgElement2);
+  
+        // Append the div to the li
+        liElement.appendChild(aInsideLi);
+  
+        liElement.appendChild(divSecond);
+        
+        // Append the li to the ul
+        ulElement.appendChild(liElement);
+      }
     }
+  
+    // Append the ul to the div
+    divElement.appendChild(continueElement);
+    divElement.appendChild(ulElement);
+    
+  
+    //fragment.appendChild(continueElement)
+    // Append the div to the fragment
+    fragment.appendChild(divElement);
+  
+    // Insert the fragment at the beginning of the <body> tag
+    document.body.insertBefore(fragment, document.body.firstChild);
   }
-
-  // Append the ul to the div
-  divElement.appendChild(ulElement);
-
-  fragment.appendChild(continueElement)
-  // Append the div to the fragment
-  fragment.appendChild(divElement);
-
-  // Insert the fragment at the beginning of the <body> tag
-  document.body.insertBefore(fragment, document.body.firstChild);
+  
 }
 
 function deleteAnimeHandler(identifier) {
@@ -83,17 +89,16 @@ function deleteAnimeHandler(identifier) {
 }
 (() => {
   let identifier, episode, videoPlayer; 
-  //when tab close, check
+  //when tab close, update last episode if necesary
   window.addEventListener('beforeunload', function (event) {
-    if(localStorage.getItem(identifier)){
-        let data  = JSON.parse(localStorage.getItem(identifier))
+    const storedData = localStorage.getItem(identifier);
+    if(storedData){
+        let data  = JSON.parse(storedData)
         data.episode = episode
         localStorage.setItem(identifier, JSON.stringify(data));
     }
-    
-});
-  
-
+  });
+  // receives information about the actual page(is home or is a episode)
   chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.type === "ROOT"){
       addElement()
@@ -110,16 +115,12 @@ function deleteAnimeHandler(identifier) {
 
     if (!addBtnExists){
         const bookmarkBtn = document.createElement("img")
-        if (localStorage.getItem(identifier) == null){
-          bookmarkBtn.src = chrome.runtime.getURL("assets/add.png");
-          bookmarkBtn.title = "Click to add anime to watching";
-          bookmarkBtn.className = "adding-btn";
-        }
-        else{
-          bookmarkBtn.src = chrome.runtime.getURL("assets/included.png");
-          bookmarkBtn.title = "Click to remove anime from watching";
-          bookmarkBtn.className = "adding-btn " + "watching";
-        }
+        
+        const isAnimeStored = localStorage.getItem(identifier) !== null;
+
+        bookmarkBtn.src = chrome.runtime.getURL(isAnimeStored ? "assets/included.png" : "assets/add.png");
+        bookmarkBtn.title = isAnimeStored ? "Click to remove anime from watching" : "Click to add anime to watching";
+        bookmarkBtn.className = "adding-btn " + (isAnimeStored ? "watching" : "");
 
         placeToInsert = document.querySelectorAll('.link_face:not(.intro)')[0];
         placeToInsert.appendChild(bookmarkBtn)
@@ -142,34 +143,32 @@ function deleteAnimeHandler(identifier) {
     
   }
 
-
   const manageAnimeEventHandler = async () => {
     const btn = document.getElementsByClassName("adding-btn")[0];
-    if(localStorage.getItem(identifier) === null){
+
+    const isAnimeStored = localStorage.getItem(identifier) !== null;
+    const imgURL = chrome.runtime.getURL(isAnimeStored ? "assets/add.png" : "assets/included.png");
+
+    if(!isAnimeStored){
         content = {
           episode: episode,
           imglink: await getLink()
         }
         localStorage.setItem(identifier, JSON.stringify(content));
-        btn.src = chrome.runtime.getURL("assets/included.png");
-        btn.className = "adding-btn " + "watching";
     }
     else{
       localStorage.removeItem(identifier);
-      btn.src = chrome.runtime.getURL("assets/add.png");
-      btn.className = "adding-btn";
     }
+    btn.src = imgURL;
+    btn.className = `adding-btn ${isAnimeStored ? "" : "watching"}`;
   }
 
   const getLink = async () => {
-    //document.querySelector(".anime_info_body_bg >img").src
-    
     const url = document.querySelector(".anime-info > a").href
     const response = await fetch(url);
     const html = await response.text();
     const tempDiv = document.createElement("div");
     tempDiv.innerHTML = html;
-    const link = tempDiv.querySelector(".anime_info_body_bg >img").src
-    return link
+    return tempDiv.querySelector(".anime_info_body_bg >img").src
   }
 })();
